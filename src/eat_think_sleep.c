1ccf66ea06aab366b6dba12add3e04c158c225e7
Miguel <miguel.srmv@gmail.com>
2023-12-21 22:50:32 +0000

    cleanup

--- src/eat_think_sleep.c
+++ src/eat_think_sleep.c
@@ -6,7 +6,7 @@
 /*   By: mde-sa-- <mde-sa--@student.42porto.com>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/12/20 11:08:23 by mde-sa--          #+#    #+#             */
-/*   Updated: 2023/12/21 21:52:14 by mde-sa--         ###   ########.fr       */
+/*   Updated: 2023/12/21 22:48:20 by mde-sa--         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,17 +18,24 @@ void	eat_routine(t_args *arg, int thread_id,
 	if (thread_id % 2 == 0)
 	{
 		take_left_fork(arg, thread_id, time_last_meal, epoch_time);
-		take_right_fork(arg, thread_id, time_last_meal, epoch_time);
+		if (arg->number_of_philos == 1)
+		{
+			ft_usleep(arg->time_to_die);
+			arg->death_flag++;
+			return ;
+		}
+		else
+			take_right_fork(arg, thread_id, time_last_meal, epoch_time);
 	}
 	else
 	{
 		take_right_fork(arg, thread_id, time_last_meal, epoch_time);
 		take_left_fork(arg, thread_id, time_last_meal, epoch_time);
 	}
-	//ft_usleep(arg->time_to_eat);
-	usleep(arg->time_to_eat);
+	ft_usleep(arg->time_to_eat);
+	//usleep(arg->time_to_eat);
 	decrement_array(arg, thread_id);
-	update_philo_state(arg, thread_id, &time_last_meal, &epoch_time);
+	update_philo_state(arg, thread_id, get_current_time(), epoch_time);
 //	print_message_helper(arg, thread_id, "Finished eating!\n");
 	pthread_mutex_unlock(&arg->forks[thread_id]);
 	pthread_mutex_unlock
@@ -44,7 +51,7 @@ void	take_left_fork(t_args *arg, int thread_id,
 	print_message_helper(arg, thread_id, message);
 	free(right_msg);
 	free(message); */
-	update_philo_state(arg, thread_id, &time_last_meal, &epoch_time);
+	update_philo_state(arg, thread_id, time_last_meal, epoch_time);
 }
 
 void	take_right_fork(t_args *arg, int thread_id,
@@ -57,7 +64,7 @@ void	take_right_fork(t_args *arg, int thread_id,
 	print_message_helper(arg, thread_id, message);
 	free(right_msg);
 	free(message); */
-	update_philo_state(arg, thread_id, &time_last_meal, &epoch_time);
+	update_philo_state(arg, thread_id, time_last_meal, epoch_time);
 }
 
 void	sleep_routine(t_args *arg, int thread_id,
@@ -65,18 +72,18 @@ void	sleep_routine(t_args *arg, int thread_id,
 {
 	size_t	remaining_time;
 
-	update_philo_state(arg, thread_id, &time_last_meal, &epoch_time);
+	update_philo_state(arg, thread_id, time_last_meal, epoch_time);
 	remaining_time = arg->time_to_die - (get_current_time() - time_last_meal);
 	if (arg->time_to_sleep < remaining_time)
-		usleep(arg->time_to_sleep);
-		//ft_usleep(arg->time_to_sleep);
+		//usleep(arg->time_to_sleep);
+		ft_usleep(arg->time_to_sleep);
 	else
-		usleep(remaining_time);
-		//ft_usleep(remaining_time);
+		//usleep(remaining_time);
+		ft_usleep(remaining_time);
 }
 
 void	think_routine(t_args *arg, int thread_id,
 	size_t time_last_meal, size_t epoch_time)
 {
-	update_philo_state(arg, thread_id, &time_last_meal, &epoch_time);
+	update_philo_state(arg, thread_id, time_last_meal, epoch_time);
 }
--- src/update_mutex.c
+++ src/update_mutex.c
@@ -6,7 +6,7 @@
 /*   By: mde-sa-- <mde-sa--@student.42porto.com>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/12/20 11:15:02 by mde-sa--          #+#    #+#             */
-/*   Updated: 2023/12/21 22:06:27 by mde-sa--         ###   ########.fr       */
+/*   Updated: 2023/12/21 22:35:36 by mde-sa--         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -26,14 +26,14 @@ void	update_count(int *count, pthread_mutex_t *count_mutex)
 	pthread_mutex_unlock(count_mutex);
 }
 
-int	update_philo_state(t_args *arg, int thread_id, size_t *time_last_meal,
-	size_t *epoch_time)
+int	update_philo_state(t_args *arg, int thread_id, size_t time_last_meal,
+	size_t epoch_time)
 {
 	pthread_mutex_lock(&arg->print_mutex);
-	if (check_death(arg, thread_id, *time_last_meal, *epoch_time)
+	if (check_death(arg, thread_id, time_last_meal, epoch_time)
 		|| arg->success_count == arg->times_each_philosopher_must_eat)
 		return (0);
-	print_message(arg, thread_id, get_current_time(), *epoch_time);
+	print_message(arg, thread_id, get_current_time(), epoch_time);
 	if (arg->philo_state[thread_id] == AVAILABLE_FOR_EATING_2_FORK_LEFT)
 		arg->philo_state[thread_id] = AVAILABLE_FOR_EATING_1_FORK_LEFT;
 	else if (arg->philo_state[thread_id] == AVAILABLE_FOR_EATING_1_FORK_LEFT)
